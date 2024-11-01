a4f119fe5657f4383c46003289c9c1e6700cec5b
alsdudrhr2 <alsdudrhr2@naver.com>
2024-11-01 18:09:13 +0900

    수정본

--- KH_Library/src/main/java/com/kh/member/controller/MemberLoginController.java
+++ KH_Library/src/main/java/com/kh/member/controller/MemberLoginController.java
@@ -1,11 +1,16 @@
 package com.kh.member.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kh.member.model.service.MemberService;
+import com.kh.member.model.vo.Member;
 
 /**
  * Servlet implementation class MemberLoginController
@@ -35,8 +40,30 @@ public class MemberLoginController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String userId = request.getParameter("userId");
+		String userPwd = request.getParameter("userPwd");
+		
+		Member loginUser = new MemberService().loginMember(userId,userPwd);
+		
+		HttpSession session = request.getSession();
+		
+		if(loginUser!=null) {
+			
+			session.setAttribute( "loginUser", loginUser);
+			session.setAttribute("alertMsg", "로그인 성공!");
+			
+		}else {
+			session.setAttribute("alertMsg","로그인 실패!");
+		}
+		
+		response.sendRedirect(request.getContextPath());
+		
+	
+		
+		
 	}
 
 }
